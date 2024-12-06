340752b36092ae8dfbbd04d18f4ccc271d41d510
Daichi Kagawa <kagawajj0930@gmail.com>
2024-12-06 09:51:34 +0900

    css_終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     def new
       @user = User.new
     end
-  
+
     def create
       @user = User.new(user_params)
       if @user.save
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
         render :new
       end
     end
-  
+
     private
-  
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
