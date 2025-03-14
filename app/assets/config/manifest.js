b040a63bb61bac1f698264dd7ac4a7ffabc9cb9e
Daichi Kagawa <kagawajj0930@gmail.com>
2025-03-14 10:55:50 +0900

    rspec_error_一時保存

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,3 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link application.css
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
-//= link controllers/radar_chart_controller.js
-//= link controllers/radar_graph_controller.js
-//= link controllers/bar_chart_controller.js
