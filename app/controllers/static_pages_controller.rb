b5a95cfc532002a204e5db351d53542a37e46d73
Daichi Kagawa <kagawajj0930@gmail.com>
2025-03-20 13:10:16 +0900

    faq_page_implement

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,9 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top terms privacy coordination]
+  skip_before_action :require_login, only: %i[top terms privacy coordination faq]
 
   def top; end
   def terms; end
   def privacy; end
   def coordination; end
+  def faq; end
 end
